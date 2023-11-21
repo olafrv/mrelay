acaa29b499e0962dee48619d9e7233360eb67cb1
Olaf Reitmaier <olafrv@gmail.com>
2023-11-21 20:07:01 +0100

    Fix body

--- test.sh
+++ test.sh
@@ -7,4 +7,4 @@ fi
 
 sendemail -v -f root@localhost \
   -t postmaster@${MRELAY_POSTFIX_DOMAIN} \
-  -u "Make Test $(date)" -m "Makes Test ($date)" $s_flag
+  -u "Make Test $(date)" -m "Makes Test $(date)" $s_flag
