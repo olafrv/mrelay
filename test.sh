04c06867b78954858b8bfe370e938fc0941ae37f
Olaf Reitmaier <olafrv@gmail.com>
2023-11-21 22:19:59 +0100

    just test with localhost

--- test.sh
+++ test.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-s_flag=""
-if [ ! -z "${MRELAY_POSTFIX_RELAYHOST}" ]; then
-  s_flag=$(echo "-s ${MRELAY_POSTFIX_RELAYHOST}" | sed 's/\[//g' | sed 's/\]//g')
-fi
-
 sendemail -v -f root@localhost \
   -t postmaster@${MRELAY_POSTFIX_DOMAIN} \
-  -u "Make Test $(date)" -m "Makes Test $(date)" $s_flag
+  -u "Make Test $(date)" -m "Makes Test $(date)"
+  -s localhost:25
