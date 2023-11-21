3c2147b942d54f53a2f8aacac14dd37a7f29a0fb
Olaf Reitmaier <olafrv@gmail.com>
2023-11-21 22:20:56 +0100

    fix bug, be explicit

--- test.sh
+++ test.sh
@@ -2,5 +2,5 @@
 
 sendemail -v -f root@localhost \
   -t postmaster@${MRELAY_POSTFIX_DOMAIN} \
-  -u "Make Test $(date)" -m "Makes Test $(date)"
+  -u "Make Test $(date)" -m "Makes Test $(date)" \
   -s localhost:25
