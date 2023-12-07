fdd1ba7ef575788c1109e4a85bd18150ae8adbc8
Olaf Reitmaier <olafrv@gmail.com>
2023-12-08 00:23:43 +0100

    Improve nginx security

--- tunnel_monitor/entrypoint.sh
+++ tunnel_monitor/entrypoint.sh
@@ -9,15 +9,55 @@ function log
 IP=$(echo $MRELAY_TUNNEL_FORWARD | cut -d':' -f1)
 PORT=$(echo $MRELAY_TUNNEL_FORWARD | cut -d':' -f2)
 
+cat - > /etc/nginx/nginx.conf <<EOF
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+events {
+        worker_connections 768;
+}
+http {
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+        ssl_prefer_server_ciphers on;
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+        gzip on;
+        include /etc/nginx/conf.d/*.conf;
+        include /etc/nginx/sites-enabled/*;
+
+        # Limit requests to 10 per second
+        limit_req_zone \$binary_remote_addr zone=mylimit:10m rate=10r/s;
+}
+EOF
+
 cat - > /etc/nginx/sites-available/default <<EOF
 server {
-    listen 443 ssl;
-    server_name ${MRELAY_POSTFIX_HOSTNAME};
+    listen 443 ssl http2;
+    server_name ${MRELAY_POSTFIX_HOSTNAME} http2;
 
     ssl_certificate /etc/letsencrypt/live/${MRELAY_POSTFIX_DOMAIN}/fullchain.pem;
     ssl_certificate_key /etc/letsencrypt/live/${MRELAY_POSTFIX_DOMAIN}/privkey.pem;
 
+    # Secure Nginx configuration
+    ssl_protocols TLSv1.2 TLSv1.3;
+
+    # Hide version
+    server_tokens off;
+
+    # Limit Request Size
+    client_max_body_size 1k;
+
     location / {
+        # Limit requests per second
+        limit_req zone=mylimit burst=10 nodelay;
+        
         root /var/www/html;
         index index.html;
     }
